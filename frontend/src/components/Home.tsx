aebd8ea278eff5050af4fcc9a2c5dfb03579c247
Jingcheng Chen <mail@jchen.ch>
2023-05-12 20:13:57 +0200

    updated logo stuff

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,3 +1,22 @@
+import { usePieces } from '../db';
+
 export function Home() {
-  return <p>Home</p>;
+  const pieces = usePieces();
+
+  return (
+    <div>
+      {pieces.map((piece) => (
+        <PiecePrieview key={piece.id} piece={piece} />
+      ))}
+    </div>
+  );
+}
+
+function PiecePrieview({ piece }: { piece: IPiece }) {
+  return (
+    <>
+      <img width='100%' src={`../pieces/${piece.id}.jpg`} alt={piece.name} />
+      <p>{piece.name}</p>
+    </>
+  );
 }
