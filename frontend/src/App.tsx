48fbe541a6d8c6e7e3c4f97f472d2249f3bfec99
Jingcheng Chen <mail@jchen.ch>
2023-05-12 17:05:05 +0200

    added firebase connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Layout from "./components/Layout";
+import { useEffect, useState } from 'react';
+import Layout from './components/Layout';
+import { onValue, ref } from '@firebase/database';
+import { addPiece, piecesRef } from './db';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [pieces, setPieces] = useState<IPiece[]>([]);
+
+  useEffect(() => {
+    onValue(piecesRef, (snapshot) => {
+      setPieces(Object.values(snapshot.val()));
+    });
+  }, []);
 
   return (
     <Layout>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
+      <div></div>
       <h1>Vite + React</h1>
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {pieces.map((piece) => (
+          <div key={piece.id}>
+            <h2>{piece.name}</h2>
+            <p>{piece.description}</p>
+          </div>
+        ))}
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </Layout>
   );
 }
