aa10e7277fb68b435e3f79db3ccc169bde6c1fd2
Bastian Nispel <nispel@codingdesign.io>
2023-05-12 14:29:07 +0200

    implemented basic layout component

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { motion, AnimatePresence, Variants } from "framer-motion";
+
+// import Footer from "./Footer";
+// import Navbar from "./Navbar";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const variants: Variants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1, transition: { duration: 0.6 } },
+  exit: { opacity: 0 },
+};
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      {/* <Navbar /> */}
+      <AnimatePresence>
+        <motion.main
+          variants={variants}
+          initial='initial'
+          animate='animate'
+          exit='exit'
+        >
+          {children}
+        </motion.main>
+      </AnimatePresence>
+      {/* <Footer /> */}
+    </>
+  );
+}
