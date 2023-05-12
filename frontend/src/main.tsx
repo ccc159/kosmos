2ecac144f94432dcc41ac3c579a3d2e6be2f3be6
Bastian Nispel <nispel@codingdesign.io>
2023-05-12 16:51:17 +0200

    React Router (#5)
    * added tailwindcss dependencies
    
    * added react-router-dom
    
    * implemented basic react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
+
 import "./index.css";
+import router from "./routes";
+import Layout from "./components/Layout";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Layout>
+      <RouterProvider router={router} />
+    </Layout>
   </React.StrictMode>
 );
