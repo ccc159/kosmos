f186f23eac514b3217731399ddbe29e0b26b050e
Bastian Nispel <nispel@codingdesign.io>
2023-05-13 08:58:44 +0200

    Chatroom (#13)
    * added basic animation
    
    * wip
    
    * implemented z-level and fixed position
    
    * fixed build issues

--- frontend/src/components/ChatRoom.tsx
+++ frontend/src/components/ChatRoom.tsx
@@ -1,19 +1,52 @@
-import { useParams } from 'react-router-dom';
-import { usePiece } from '../db';
-import Exhibit from './Exhibit';
-import CommentBox from './CommentBox';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { motion } from "framer-motion";
+
+import Exhibit from "./Exhibit";
+import { usePiece } from "../db";
+import CommentBox from "./CommentBox";
+
+// const variants = {
+//   initial: { paddingTop: 0 },
+//   animate: { paddingTop: "50vh" },
+// };
 
 export function ChatRoom() {
   const { id } = useParams();
+  const [extended, setExtended] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setExtended(false);
+      } else {
+        setExtended(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const piece = usePiece(id as string);
-
   if (!piece) return <p>Piece not found</p>;
 
   return (
-    <div className='flex flex-col w-full h-full'>
-      <Exhibit piece={piece} />
+    <motion.div
+      className='flex flex-col w-full h-full'
+      onScroll={() => setExtended(!extended)}
+    >
+      <Exhibit piece={piece} extended={extended} />
+      {/* <button onClick={() => setExtended(!extended)}>EXTEND</button> */}
+      {/* <motion.div
+        variants={variants}
+        initial={extended ? "animate" : "initial"}
+        animate={extended ? "initial" : "animate"}
+      ></motion.div> */}
+      {!extended && <div className='flex w-screen h-[50vh] bg-white' />}
       <CommentBox piece={piece} />
-    </div>
+    </motion.div>
   );
 }
