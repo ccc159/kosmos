4485c6e903b4b181523f2795f11586dca055cbe5
Jingcheng Chen <mail@jchen.ch>
2023-05-13 09:10:22 +0200

    added menu

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,13 +1,17 @@
 import { Link } from 'react-router-dom';
 import { usePieces } from '../db';
 import { IPiece } from '../interfaces/piece';
+import menu from '../icons/menu.svg';
 
 export function Home() {
   const pieces = usePieces();
 
   return (
     <div>
-      <h1 className='text-2xl font-bold fixed top-0 h-14 w-screen bg-white flex items-center '>Paul Klee Kosmos</h1>
+      <h1 className='text-2xl font-bold fixed top-0 h-14 w-screen bg-white left-0 flex items-center justify-center p-[1rem]'>
+        <span className='flex items-start w-[800px]'>Paul Klee Kosmos</span>
+        <img className='w-10 ml-4' src={menu} alt='logo' />
+      </h1>
       <div className='piece-container'>
         {pieces
           .filter((p) => p.date_of_creation !== undefined)
